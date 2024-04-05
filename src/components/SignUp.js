c216588d5d021ddf618212311cdd51368cd189bb
Susanto <sushantomahato1@gmail.com>
2024-04-05 14:29:58 +0530

    feat: password reset functionality

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -9,6 +9,7 @@ const SignUp = () => {
   const [isLoginForm, setIsLoginForm] = useState(false);
   const ctx = useContext(appContext);
   const nav = useNavigate();
+  const [isForgetPassword, setIsForgetPassword] = useState(false);
 
   const signUpFunction = async () => {
     try {
@@ -66,26 +67,68 @@ const SignUp = () => {
       nav("/");
     } catch (err) {
       console.log(err);
+      alert(err);
+    }
+  };
+  const resetPassword = async () => {
+    try {
+      const response = await fetch(
+        "https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyC7sVHD3PsWo_Lma3A_MJKwkcCo-BjiFm8",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            requestType: "PASSWORD_RESET",
+            email: emailRef.current.value,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const data = await response.json();
+      alert("A link has been send to your email to reset password");
+      console.log(data);
+      setIsForgetPassword(false);
+      if (!response.ok) {
+        throw new Error(response.message);
+      }
+    } catch (err) {
+      alert(err.message);
     }
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    if (isLoginForm) {
-      signInFunction();
+    if (isForgetPassword) {
+      resetPassword();
     } else {
-      if (passwordRef.current.value === confirmPasswordRef.current.value) {
-        signUpFunction();
+      if (isLoginForm) {
+        signInFunction();
       } else {
-        alert("Different password input");
+        if (passwordRef.current.value === confirmPasswordRef.current.value) {
+          signUpFunction();
+        } else {
+          alert("Different password input");
+        }
       }
     }
+    emailRef.current.value = "";
+    passwordRef.current.value = "";
+    confirmPasswordRef.current.value = "";
+  };
+
+  const handleForgot = () => {
+    setIsForgetPassword(true);
   };
   return (
     <div className="w-[40%] mx-auto shadow-lg p-8 my-24">
       <h1 className="font-bold text-2xl my-4">
-        {isLoginForm ? "Login" : "SignUp"}
+        {!isForgetPassword
+          ? isLoginForm
+            ? "Login"
+            : "SignUp"
+          : "Reset Password"}
       </h1>
       <form className="flex flex-col " onSubmit={handleSubmit}>
         <input
@@ -95,14 +138,16 @@ const SignUp = () => {
           ref={emailRef}
           required
         />
-        <input
-          type="password"
-          placeholder="Password"
-          className="border border-gray-400 p-2 text-lg rounded-lg my-3"
-          required
-          ref={passwordRef}
-        />
-        {!isLoginForm && (
+        {!isForgetPassword && (
+          <input
+            type="password"
+            placeholder="Password"
+            className="border border-gray-400 p-2 text-lg rounded-lg my-3"
+            required
+            ref={passwordRef}
+          />
+        )}
+        {!isForgetPassword && !isLoginForm && (
           <input
             type="password"
             placeholder="Confirm Password"
@@ -112,15 +157,26 @@ const SignUp = () => {
           />
         )}
         <button className="bg-blue-300 p-2 rounded-lg text-lg my-4">
-          {isLoginForm ? "Login" : "Sign Up"}
+          {isForgetPassword ? "Send Link" : isLoginForm ? "Login" : "Sign Up"}
         </button>
       </form>
+      {!isForgetPassword && (
+        <p
+          className="cursor-pointer hover:text-blue-500"
+          onClick={handleForgot}
+        >
+          Forgot Password?
+        </p>
+      )}
 
       <p className="my-12 bg-gray-300 p-3 rounded-lg">
         {isLoginForm ? "Don't have an Account?" : "Already Have an Account! "}{" "}
         <span
           className="font-bold cursor-pointer hover:text-blue-400"
-          onClick={() => setIsLoginForm((prev) => !prev)}
+          onClick={() => {
+            setIsLoginForm((prev) => !prev);
+            setIsForgetPassword(false);
+          }}
         >
           {isLoginForm ? "Sign Up" : "Sign in"}
         </span>
