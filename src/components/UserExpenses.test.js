44a1c9a75daace93138be956f192656e88bc49a9
Susanto <sushantomahato1@gmail.com>
2024-04-11 23:54:21 +0530

    async test

--- src/components/UserExpenses.test.js
+++ src/components/UserExpenses.test.js
@@ -7,3 +7,15 @@ describe("is user expenses visible", () => {
     expect(htmlElement).toBeInTheDocument();
   });
 });
+
+describe("async code", () => {
+  test("renders posts if request succeed", async () => {
+    window.fetch = jest.fn();
+    window.fetch.mockResolveValueOnce({
+      json: async () => [],
+    });
+    render(<UserExpenses />);
+    const listItemEl = await screen.findAllByRole("listitem");
+    expect(listItemEl).not.toHaveLength(0);
+  });
+});
